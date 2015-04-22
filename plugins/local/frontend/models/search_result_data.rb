fcd105acec3bf1099d7f0bad67577cfd74a6fc0c
Noah Huffman <noah.huffman@gmail.com>
2015-04-22 13:08:52 -0400

    changes to resource and subject facets

--- plugins/local/frontend/models/search_result_data.rb
+++ plugins/local/frontend/models/search_result_data.rb
@@ -9,7 +9,15 @@ class SearchResultData
 
 # this changes what facets are requested for Resource searches. Display order follows the order in the array. 
  def self.ACCESSION_FACETS
-   ["accession_date_year", "creators", "subjects"]
+   ["accession_date_year","creators","subjects"]
  end
 
+ def self.RESOURCE_FACETS
+   ["level","publish","creators"]
+ end
+ 
+ def self.SUBJECT_FACETS
+   ["first_term_type","source"]
+ end
+ 
 end
