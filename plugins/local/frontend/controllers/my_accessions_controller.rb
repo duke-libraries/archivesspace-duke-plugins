a88920d79d6fc0980dce0bd0ab1f4ed7d8ecd128
Noah Huffman <noah.huffman@gmail.com>
2015-07-30 08:40:59 -0400

    added controllers to refine default sort behavior
    changes default browse sort to "modified descending" for accessions and
    resources

---
+++ plugins/local/frontend/controllers/my_accessions_controller.rb
@@ -0,0 +1,15 @@
+# plugins/local/frontend/controllers/my_resources_controller.rb
+# just adds a default sort param to the index action of the accession_controller
+# can do a similar tweak to others, like accessions. 
+
+require Rails.root.join('app/controllers/accessions_controller')
+
+class AccessionsController < ApplicationController
+
+
+  def index
+    params["sort"] ||= "user_mtime desc"
+     @search_data = Search.for_type(session[:repo_id], "accession", params_for_backend_search.merge({"facet[]" => SearchResultData.ACCESSION_FACETS}))
+  end
+
+end
