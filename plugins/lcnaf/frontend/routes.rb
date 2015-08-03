2935ab724a411fbb5ddb8ede1bcb13073a1a2883
Noah Huffman <noah.huffman@gmail.com>
2015-08-03 12:06:45 -0400

    remove lcnaf plugin
    ASpace ships with lcnaf plugin by default, remove this local copy

--- plugins/lcnaf/frontend/routes.rb
+++
@@ -1,7 +0,0 @@
-ArchivesSpace::Application.routes.draw do
-
-  match('/plugins/lcnaf' => 'lcnaf#index', :via => [:get])
-  match('/plugins/lcnaf/search' => 'lcnaf#search', :via => [:get])
-  match('/plugins/lcnaf/import' => 'lcnaf#import', :via => [:post])
-
-end
